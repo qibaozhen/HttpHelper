96bc3f2709989ce4b528ef11d2f0f196b5a40d88
齐春宝 <@qqbbzz@126.com>
2016-11-25 10:19:05 +0800

    第一次提交

---
+++ HttpHelper/HttpHelper/http/HttpHelper.swift
@@ -0,0 +1,15 @@
+//
+//  HttpHelper.swift
+//  HttpHelper
+//
+//  Created by Rain on 2016/11/25.
+//  Copyright © 2016年 Rain. All rights reserved.
+//
+
+import UIKit
+
+class HttpHelper: NSObject {
+   class func run()  {
+        print("run.....")
+    }
+}
